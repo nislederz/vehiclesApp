8d72fb17e6ba0259fb30cafbde856b549e5b7d48
nislederz <nislederz@gmail.com>
2021-09-21 13:11:23 -0500

    style login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,12 +8,52 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Login Screen")
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [           
+            _showLogo(),
+            _showEmail(),
+          ],
+        )
       ),
     );
   }
+
+  Widget _showLogo (){
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 300,
+    );
+  }
+
+  Widget _showEmail (){
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa tu email.',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value){
+          _email = value;
+          print(_email);
+        },
+      ),
+    );
+  }
+
 }
+
+

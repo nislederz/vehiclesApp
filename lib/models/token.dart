599dfb530d4ab66f41c870cf25ad495b434c539b
nislederz <nislederz@gmail.com>
2021-10-03 20:36:58 -0500

    add new models

--- lib/models/token.dart
+++ lib/models/token.dart
@@ -2,22 +2,24 @@ import 'package:vehicles_app/models/document_type.dart';
 import 'package:vehicles_app/models/user.dart';
 
 class Token {
-  String token ="";
-  String expiration ="";
-  User user =User(
-    firstName: "", 
-    lastName: "", 
-    documentType: DocumentType(id: 0, description: ""), 
-    document: "",
-    address: "",
-    imageId: "",
-    imageFullPath: "",
-    userType: 0,
-    fullName: "", 
-    id:"",
-    userName: "",
-    email: "",
-    phoneNumber: "",
+  String token = '';
+  String expiration = '';
+  User user = User(
+    firstName: '', 
+    lastName: '', 
+    documentType: DocumentType(id: 0, description: ''), 
+    document: '', 
+    address: '', 
+    imageId: '', 
+    imageFullPath: '', 
+    userType: 0, 
+    fullName: '', 
+    id: '', 
+    userName: '', 
+    email: '', 
+    phoneNumber: '', 
+    vehicles: [], 
+    vehiclesCount: 0
   );
 
   Token({required this.token, required this.expiration, required this.user});
